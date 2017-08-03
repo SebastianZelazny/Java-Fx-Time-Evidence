3d6c7295eb688cc2e6e83cd562daff7d5c960200
Sebastian <sebastianzelazny@poczta.fm>
2017-08-03 22:51:39 +0200

    Ukonczony modul usera

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
+
+import com.mysql.fabric.xmlrpc.base.Data;
 
 import dbConnect.DBConn;
 import javafx.collections.FXCollections;
@@ -30,6 +33,8 @@ public class UserController {
     @FXML
     private Menu MRozlicz;
     
+    @FXML
+    private GridPane GPDolary;
     
     @FXML
     private GridPane GPSettle;
@@ -67,6 +72,7 @@ public class UserController {
     
     public void initialize()
     {
+    	GPDolary.setVisible(false);
     	conDB = new DBConn();
     	CBMonth.getItems().addAll("01","02","03","04","05","06","07","08","09","10","11","12");
     	CBMonth.getSelectionModel().selectFirst();
@@ -80,6 +86,7 @@ public class UserController {
     	ResultSet rs;
     	String SelectID = "Select ID_Emplo,MaterialPrepare,scholing,delegation from employee,logins where login='"+lc.getLogin()+"' and employee.ID_Login = logins.ID_L;";
     	int IndeksEmplo = 0;
+    	int countMounth = 0;
     	System.out.println("Login: "+lc.getLogin());
     	
     	try {
@@ -99,48 +106,76 @@ public class UserController {
 			e1.printStackTrace();
 		}
     	
-    	String InsertEvidence = "Insert into evidence (ID_Emp,Month,Number_H_Mat,Number_H_Sch,Number_H_Del) values (?,?,?,?,?)";
+ 
     	
-   
-    /*	try {
+    	String SelectMonth = "select count(month) as Liczba_miesiecy from evidence where id_emp = "+IndeksEmplo+" and month ="+CBMonth.getValue()+";";
+    	try
+    	{
+    		rs = null;
+    		rs = con.createStatement().executeQuery(SelectMonth);
+    		while(rs.next())
+    		{
+    			countMounth = rs.getInt(1);
+    			System.out.println(countMounth);
+    		}
+    	}
+    	catch (SQLException e) {
+			// TODO: handle exception
+		}
+    	if(countMounth>0)
+    	{
+    		System.out.println("Ju¿ rozlicza³eœ siê w tym miesi¹cu jeœli chcesz wprowadziæ poprawki skontaktuj siê z Administratorem");
+    	}
+    	else
+    	{
+    	   	String InsertEvidence = "Insert into evidence (ID_Emp,Month,Number_H_Mat,Number_H_Sch,Number_H_Del,Ear$Materials,Ear$Schooling,Ear$Delegations,Ear$AllNetto,Ear$AllBrutto) values (?,?,?,?,?,?,?,?,?,?)";
     		
-    	 	preparedStmt = con.prepareStatement(InsertEvidence);
-    		preparedStmt.setInt(1, IndeksEmplo);
-			preparedStmt.setString(2, String.valueOf(CBMonth.getValue()));
-			preparedStmt.setString(3, TFMaterials.getText());
-	    	preparedStmt.setString(4, TFSchool.getText());
-	    	preparedStmt.setString(5, TFDelega.getText());
-	    	preparedStmt.execute();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-    	
-    	System.out.println("delegations: "+WorkingSchedlueDelegations);
-		System.out.println("Materials: "+WorkingSchedlueMaterials);
-		System.out.println("Schooling: "+WorkingSchedlueSchooling);
-		
-		System.out.println("delegations: "+TFDelega.getText());
-		System.out.println("Materials: "+TFMaterials.getText());
-		System.out.println("Schooling: "+TFSchool.getText());
-    	double resultMat = Double.valueOf(TFMaterials.getText())*WorkingSchedlueMaterials;
-    	double resultScho = Double.valueOf(TFSchool.getText())*WorkingSchedlueSchooling;
-    	double resultDele = Double.valueOf(TFDelega.getText())*WorkingSchedlueDelegations;
-    	double netto  = resultDele +resultMat + resultScho;
-    	double brutto = (netto * 0.23) + netto;
-    	Lbl$Materials.setText(String.valueOf(Double.valueOf(TFMaterials.getText())*WorkingSchedlueMaterials));
-    	Lbl$Schooling.setText(String.valueOf(Double.valueOf(TFSchool.getText())*WorkingSchedlueSchooling));
-    	Lbl$delegations.setText(String.valueOf(Double.valueOf(TFDelega.getText())*WorkingSchedlueDelegations));
-    	Lbl$Netto.setText(String.valueOf(netto));
-    	Lbl$Brutto.setText(String.valueOf(brutto));
-    	
+    		double resultMat = Double.valueOf(TFMaterials.getText())*WorkingSchedlueMaterials;
+        	double resultScho = Double.valueOf(TFSchool.getText())*WorkingSchedlueSchooling;
+        	double resultDele = Double.valueOf(TFDelega.getText())*WorkingSchedlueDelegations;
+        	double netto  = resultDele +resultMat + resultScho;
+        	double brutto = (netto * 0.23) + netto;
+        	
+    		try {
+        	 	preparedStmt = con.prepareStatement(InsertEvidence);
+        		preparedStmt.setInt(1, IndeksEmplo);
+    			preparedStmt.setString(2, String.valueOf(CBMonth.getValue()));
+    			preparedStmt.setString(3, TFMaterials.getText());
+    	    	preparedStmt.setString(4, TFSchool.getText());
+    	    	preparedStmt.setString(5, TFDelega.getText());
+    	    	preparedStmt.setDouble(6, resultMat);
+    	    	preparedStmt.setDouble(7, resultScho);
+    	    	preparedStmt.setDouble(8, resultDele);
+    	    	preparedStmt.setDouble(9, netto);
+    	    	preparedStmt.setDouble(10, brutto);
+    	    	preparedStmt.execute();
+    		} catch (SQLException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
+    		
+    		System.out.println("delegations: "+WorkingSchedlueDelegations);
+    		System.out.println("Materials: "+WorkingSchedlueMaterials);
+    		System.out.println("Schooling: "+WorkingSchedlueSchooling);
+    		
+    		System.out.println("delegations: "+TFDelega.getText());
+    		System.out.println("Materials: "+TFMaterials.getText());
+    		System.out.println("Schooling: "+TFSchool.getText());
+        	Lbl$Materials.setText(String.valueOf(Double.valueOf(TFMaterials.getText())*WorkingSchedlueMaterials));
+        	Lbl$Schooling.setText(String.valueOf(Double.valueOf(TFSchool.getText())*WorkingSchedlueSchooling));
+        	Lbl$delegations.setText(String.valueOf(Double.valueOf(TFDelega.getText())*WorkingSchedlueDelegations));
+        	Lbl$Netto.setText(String.valueOf(netto));
+        	Lbl$Brutto.setText(String.valueOf(brutto));
+        	
+        	GPDolary.setVisible(true);
+        	GPSettle.setVisible(false);
+        	BtnConvert.setVisible(false);
+    	}
     	
     	TFDelega.setText("");
     	TFMaterials.setText("");
     	TFSchool.setText("");
-    	
     	CBMonth.getSelectionModel().selectFirst(); 	
-    	
     }
     @FXML
     void ShowMenuSettleUp(ActionEvent event) {
